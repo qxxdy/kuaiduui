190534809eeaee2980b7e4b029c28b48a4fbb9d5
xdy <3074617926@qq.com>
2024-04-04 00:12:29 +0800

    feat: 完成投递记录前端

---
+++ src/views/self/history/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>
+          <h3>投递岗位名称</h3>
+          招聘类型：
+          <el-divider direction="vertical"></el-divider>
+          投递时间：
+        </span>
+      </div>
+      <el-steps :active="active" finish-status="success">
+        <el-step title="步骤 1"></el-step>
+        <el-step title="步骤 2"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+
+  },
+  data() {
+    return {
+      active: 0
+    }
+  },
+  methods: {}
+}
+</script>
